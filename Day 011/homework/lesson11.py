9ecf0e821d6ab7e5727aacd79087b0fe321a0708
Anastasiakhvedelidze <anastasiakhvedelidze06@gmail.com>
2024-04-06 20:21:44 +0400

    commit

--- Day 011/homework/lesson11.py
+++ Day 011/homework/lesson11.py
@@ -2,10 +2,10 @@ Mom_age = (input("how old is your mother?: "))
 Dad_age = (input("how old is your father?: "))
 
 if Mom_age > Dad_age:
- print("დედაშენი დიდია მამაშენზე")   
+    print("დედაშენი დიდია მამაშენზე")   
 
 if Dad_age > Mom_age:
- print("მამაშენი დიდია დედაშნზე")
+    print("მამაშენი დიდია დედაშნზე")
 
 if Mom_age == Dad_age:
- print("ისინი ტოლები არიან")
+    print("ისინი ტოლები არიან")
