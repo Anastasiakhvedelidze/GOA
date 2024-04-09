a1a0ac1d24b9aa22ea339f14a1f3e16c6ca20313
Anastasiakhvedelidze <anastasiakhvedelidze06@gmail.com>
2024-04-09 22:06:39 +0400

    commit

--- Day 010/homework/lesson10.py
+++
@@ -1,8 +0,0 @@
-queston1 = bool (input("type first answer (true, false):  "))
-queston2 = bool(input("type second answer:  "))
-
-answer1 = queston1 and queston2
-answer2 = queston1 or queston2
-
-print(answer1)
-print(answer2)
